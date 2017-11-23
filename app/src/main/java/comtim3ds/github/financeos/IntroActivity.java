1cd0136d386ba60b9ed9c0ec4d694509bf2e21a2
Tim3Ds@gmail.com <Tim3Ds@gmail.com>
2017-11-23 14:12:26 -0700

    Intro

---
+++ app/src/main/java/comtim3ds/github/financeos/IntroActivity.java
@@ -0,0 +1,13 @@
+package comtim3ds.github.financeos;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class IntroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+    }
+}
--- app/src/main/java/comtim3ds/github/financeos/MainActivity.java
+++ app/src/main/java/comtim3ds/github/financeos/MainActivity.java
@@ -1,13 +1,38 @@
 package comtim3ds.github.financeos;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText mTextEntry;
+    private Button mBTNsendText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // link variables to View
+        mBTNsendText = (Button) findViewById(R.id.btn_send_entry_to_child);
+        mTextEntry = (EditText) findViewById(R.id.et_text_entry);
+
+        // set listener
+        mBTNsendText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Context context = MainActivity.this;
+
+            }
+        });
     }
 }
