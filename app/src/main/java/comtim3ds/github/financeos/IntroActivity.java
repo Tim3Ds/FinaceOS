be347f989bed9f9ef3c21c4278d6ebc8b4e079d9
Tim3Ds@gmail.com <Tim3Ds@gmail.com>
2017-11-25 10:08:04 -0700

    add pages still need add to table page

--- app/src/main/java/comtim3ds/github/financeos/IntroActivity.java
+++ app/src/main/java/comtim3ds/github/financeos/IntroActivity.java
@@ -1,13 +1,36 @@
 package comtim3ds.github.financeos;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class IntroActivity extends AppCompatActivity {
+
+    Button goMain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
+
+        goMain = (Button) findViewById(R.id.btn_goToMain);
+
+        goMain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Context context = IntroActivity.this;
+
+                Class destination = MainActivity.class;
+
+                Intent startMain = new Intent(context, destination);
+
+                startActivity(startMain);
+
+            }
+        });
     }
 }
--- app/src/main/java/comtim3ds/github/financeos/MainActivity.java
+++ app/src/main/java/comtim3ds/github/financeos/MainActivity.java
@@ -9,30 +9,41 @@ import android.text.Editable;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText mTextEntry;
-    private Button mBTNsendText;
+//    TextView Expense;
+//    TextView Income;
+//    TextView Over_Short;
+
+    Button goToExpenseList;
+    Button goTOIncomeList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // link variables to View
-        mBTNsendText = (Button) findViewById(R.id.btn_send_entry_to_child);
-        mTextEntry = (EditText) findViewById(R.id.et_text_entry);
+        goToExpenseList = (Button) findViewById(R.id.btn_goToExpenses);
 
-        // set listener
-        mBTNsendText.setOnClickListener(new View.OnClickListener() {
+        goToExpenseList.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                Context context = MainActivity.this;
-
+                Intent startExpenseList = new Intent(MainActivity.this, ExpenseActivity.class);
+                startActivity(startExpenseList);
             }
         });
+
+        goTOIncomeList = (Button) findViewById(R.id.btn_goToIncome);
+        goTOIncomeList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent startIncomeList = new Intent(MainActivity.this, IncomeActivity.class);
+                startActivity(startIncomeList);
+            }
+        });
+
     }
 }
